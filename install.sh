5129104f55707ed0aaeb208c1ebe251cadc1ec8b
tscrond <tomek.skrond@gmail.com>
2026-06-22 14:32:33 +0200

    install.sh fix

--- install.sh
+++ install.sh
@@ -2,148 +2,140 @@
 set -euo pipefail
 trap 'echo "FAILED at line $LINENO"' ERR
 
-### HELPERS ###
-check_if_installed() {
-    local program="$1"
+### LOG ###
+log() {
+    local t="${1:-info}"
+    shift
+    echo "[$t] $*"
+}
 
-    if command -v "$program" >/dev/null 2>&1; then
-        log success "$program installed - proceeding"
-        return 0
-    else
-        log error "$program not installed - aborting"
-        return 1
-    fi
+### CHECKS ###
+is_installed() {
+    command -v "$1" >/dev/null 2>&1
 }
 
 require_or_install() {
-    local pkg="$1"
+    local cmd="$1"
+    local pkg="${2:-$1}"
 
-    if command -v "$pkg" >/dev/null 2>&1; then
-        log success "$pkg already installed"
+    if is_installed "$cmd"; then
+        log success "$cmd already installed"
     else
-        log error "$pkg missing - installing"
-        sudo pacman -S --noconfirm "$pkg"
+        log error "$cmd missing - installing $pkg"
+        sudo pacman -S --noconfirm --needed "$pkg"
     fi
 }
 
-log() {
-    local MSG_TYPE="${1:-info}"
-    shift
-    local MESSAGE="$*"
+clone_if_missing() {
+    local repo="$1"
+    local dest="$2"
 
-    local available_msg=("info" "error" "success")
-    local valid=0
-
-    for m in "${available_msg[@]}"; do
-        if [[ "$MSG_TYPE" == "$m" ]]; then
-            valid=1
-            break
-        fi
-    done
-
-    [[ $valid -eq 0 ]] && MSG_TYPE="unknown"
-    
-    echo "[${MSG_TYPE}] ${MESSAGE}"
-}
-
-paru_needed() {
-    if ! check_if_installed paru; then
-        log error "cannot proceed further - make sure paru is installed"
-        exit 1
+    if [[ -d "$dest" ]]; then
+        log success "$dest already exists - skipping clone"
+    else
+        git clone "$repo" "$dest"
     fi
 }
 
+### PREREQUISITES ###
 prerequisites() {
     if ! pacman --version >/dev/null 2>&1; then
-        log error "pacman not working - aborting"
+        log error "pacman not working"
         exit 1
     fi
 
-    sudo pacman -Syyu
-    
-    require_or_install git
+    sudo pacman -Syu --noconfirm
+
+    require_or_install git git
 }
 
-### INSTALLATION HELPERS ###
-enable_hyprpolkitagent() {
-    systemctl --user enable --now hyprpolkitagent.service
-}
+### OH MY ZSH (IDEMPOTENT) ###
+install_ohmyzsh() {
+    if [[ -d "$HOME/.oh-my-zsh" ]]; then
+        log success "Oh My Zsh already installed"
+        return
+    fi
 
-enable_xdgdesktopportal() {
-    systemctl --user enable --now xdg-desktop-portal-hyprland
-}
-
-### INSTALLATION FUNCTIONS ###
-initial() {
-    # install tooling like openssh, ohmyzsh, vim etc.
-    sudo pacman -Syu --noconfirm fzf \
-        hyprland \
-        kitty \
-        sddm \
-        hyprlock \
-        yazi \
-        brightnessctl \
-        hyprpolkitagent \
-        xdg-desktop-portal-hyprland \
-        waybar \
-        unzip \
-        zip \
-        p7zip \
-        wget \
-        curl \
-        git \
-        htop \
-        fastfetch \
-        wl-clipboard \
-        stow
-
-    enable_hyprpolkitagent
-    enable_xdgdesktopportal
-
+    RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 }
 
-brave() {
-    paru_needed
-    paru -Syu brave
+### SERVICES ###
+enable_service() {
+    systemctl --user enable --now "$1" 2>/dev/null || true
 }
 
+### INITIAL SETUP ###
+initial() {
+    sudo pacman -S --noconfirm --needed \
+        fzf hyprland kitty sddm hyprlock yazi brightnessctl \
+        hyprpolkitagent xdg-desktop-portal-hyprland waybar \
+        unzip zip p7zip wget curl git htop fastfetch \
+        wl-clipboard stow
+
+    enable_service hyprpolkitagent.service
+    enable_service xdg-desktop-portal-hyprland.service
+
+    install_ohmyzsh
+
+    clone_if_missing \
+        https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+
+    clone_if_missing \
+        https://github.com/zsh-users/zsh-autosuggestions.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+
+    clone_if_missing \
+        https://github.com/zsh-users/zsh-history-substring-search.git \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-history-substring-search"
+}
+
+### AUR HELPER ###
 aur_helper() {
-    sudo pacman -Syu --noconfirm --needed base-devel
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg -si
+    if is_installed paru; then
+        log success "paru already installed"
+        return
+    fi
+
+    sudo pacman -S --noconfirm --needed base-devel
+
+    git clone https://aur.archlinux.org/paru.git /tmp/paru
+
+    pushd /tmp/paru >/dev/null
+    makepkg -si --noconfirm
+    popd >/dev/null
 }
 
+### BRAVE ###
+brave() {
+    if is_installed brave; then
+        log success "brave already installed"
+        return
+    fi
+
+    paru -S --noconfirm brave
+}
+
+### WALKER ###
 walker() {
-    # deps
-    # - GTK4 development files
-    # - gtk4-layer-shell development files
-    # - Protocol Buffers compiler (protoc)
-    # - cairo development files
-    # - poppler-glib development files
-    # - Rust toolchain (via rustup)
+    sudo pacman -S --noconfirm --needed \
+        gtk4 gtk4-layer-shell cairo poppler-glib protobuf
 
-    sudo pacman -Syu --noconfirm gtk4 gtk4-layer-shell cairo poppler-glib protobuf
-
-    paru_needed
-    
-    paru -Syu walker \
-        elephant
+    paru -S --noconfirm walker elephant
 }
 
 walker_post() {
-    check_if_installed elephant
-    
-    elephant service enable
-    systemctl --user start elephant.service
+    if ! is_installed elephant; then
+        log error "elephant missing"
+        return 1
+    fi
+
+    elephant service enable || true
+    systemctl --user start elephant.service || true
 }
 
-### MAIN FLOW ###
+### MAIN ###
 prerequisites
 initial
 aur_helper
